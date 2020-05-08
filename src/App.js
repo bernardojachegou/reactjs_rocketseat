f2800bb7c5cc3d15f85b5f828acc68e37fa8b02f
Michel Bernardo <bernardoenglish@outlook.com>
2020-05-07 23:26:29 -0400

    navegação por detalhes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
+import Routes from './routes';
 
 import './style.css';
 
 import Header from './components/Header';
-import Main from './pages/main';
 
 const App = () => (
   <div className="App">
     <Header />
-    <Main />
+    <Routes />
   </div>
 );
 
